d6f25e9c88165463e3447ef1f954e712985fff1c
pranav-vaniya <pmvaniya16@gmail.com>
2024-08-25 20:17:34 +0530

    Updated .gitignore

--- extract_tar_files.py
+++ extract_tar_files.py
@@ -1,7 +1,18 @@
 import os
 import tarfile
 from shutil import rmtree
-from constants import *
+from pathlib import Path
+
+# this program expects all the tar files in a directory called OpenWebText
+ROOT_DIR = Path(__file__).parent
+TAR_DIR = ROOT_DIR / "OpenWebText"
+DATA_DIR = ROOT_DIR / "data"
+
+INFO_M = "\033[34m  INFO    \033[0m"
+WARN_M = "\033[33m  WARN    \033[0m"
+DANGER_M = "\033[31m DANGER   \033[0m"
+SUCCESS_M = "\033[32m  SUCC    \033[0m"
+QUESTION_M = "\033[35m  QUES    \033[0m"
 
 # a directory created on extracting an OpenWebText tar file
 openwebtext_path = DATA_DIR / "openwebtext"
